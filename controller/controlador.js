57329f2dda9fd51310c1db9886ceda2a27c2a9e7
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2024-03-04 02:49:45 -0600

    CRUD USUARIOS

--- controller/controlador.js
+++ controller/controlador.js
@@ -52,25 +52,40 @@ export const editarUsuario = async (req, res) => {
  
 
 
-    // export const eliminarUsuario=async(req,res)=>{
-    //     try {
-    //         const [result]=await pool.query('DELETE FROM usuarios WHERE id=?',[req.params.id])
-    //         //    console.log(result);
-    //             if (result.affectedRows <= 0) return res.status(404).json ({
-    //                 message:"No se encontro usuario a eliminar"
-    //             })
-    //         //    res.send("usuario eliminado");
-    //         res.sendStatus(204)
-    //     } catch (error) {
-    //         return res.status(500).json({
-    //         message:'somenthing went wrong'
-    //     })
-    //     }
-    // }
+
+    export const eliminarUsuario = async (req, res) => {
+
+        if (req.session.rol == 'admin') {
+            const { id } = req.params;
+            const [result]=await pool.query('DELETE FROM users WHERE id=?',[id])
+        //otro if de si es mayor a 0?
+        if (result && result.affectedRows > 0) {
+            const [rows]=await pool.query('SELECT * FROM users');
+        res.render('usuarios', {
+            alert: true,
+            alertTitle: "Eliminado",
+            alertMessage: "¡Eliminado Exitoso",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            login: true,
+            roluser: true,
+            name: req.session.name,
+            rol: req.session.rol,
+            usuarios:rows,
+            ruta:'usuarios'
+        });
+    } }else{
+        (error) 
+            console.error(error);
+            // Manejar el error apropiadamente
+            res.status(500).send('Error interno del servidor');
+        }
+    }
 
 
 export const methods = {
     usuarios,
     editarUsuario,
-
+    eliminarUsuario
   }
