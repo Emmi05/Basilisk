fb7f94dab6d37570143571fb7cb6f4cc2a3c6549
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2024-03-04 21:08:45 -0600

    crud usuario y cliente listos falta validaciones

--- controller/controlador.js
+++ controller/controlador.js
@@ -43,8 +43,7 @@ export const editarUsuario = async (req, res) => {
             ruta:'usuarios'
         });
     } }else{
-        (error) 
-            console.error(error);
+        
             // Manejar el error apropiadamente
             res.status(500).send('Error interno del servidor');
         }
@@ -76,6 +75,94 @@ export const editarUsuario = async (req, res) => {
             ruta:'usuarios'
         });
     } }else{
+        // (error) 
+        //     console.error(error);
+            // Manejar el error apropiadamente
+            res.status(500).send('Error interno del servidor');
+        }
+    }
+
+
+    // CLIENTES
+    export const crearCliente= async (req, res) => {
+        try {
+            const { name, cel, conyuge_name, conyuge_cel, adress } = req.body;
+            await pool.query('INSERT INTO customer SET ?', { name, cel, conyuge_name, conyuge_cel,adress });
+            
+            res.render('register', {
+                alert: true,
+                alertTitle: "Registro",
+                alertMessage: "¡Registro Exitoso",
+                alertIcon: 'success',
+                showConfirmButton: false,
+                timer: 1500,
+                ruta: '/', 
+                login: true,
+                roluser: true,
+                name: req.session.name,
+                rol: req.session.rol,
+            });
+        } catch (error) {
+            console.error(error);
+            // Manejar el error apropiadamente
+            res.status(500).send('Error interno del servidor');
+        }
+    
+    }
+
+    export const editarClientes = async (req, res) => {
+
+        if (req.session.rol == 'admin') {
+            const { id } = req.params;
+            const { name, cel, conyuge_name, conyuge_cel, adress } = req.body;
+            const [result] = await pool.query('UPDATE customer SET name = IFNULL (?, name), cel = IFNULL (?, cel), conyuge_name = IFNULL (?, conyuge_name), conyuge_cel = IFNULL (?, conyuge_cel), adress= IFNULL (?, adress) WHERE id = ?', [name, cel, conyuge_name,conyuge_cel, adress, id]);
+        //otro if de si es mayor a 0?
+        if (result && result.affectedRows > 0) {
+            const [rows]=await pool.query('SELECT * FROM customer');
+        res.render('clientes', {
+            alert: true,
+            alertTitle: "Actualización",
+            alertMessage: "¡Actualización Exitoso",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            login: true,
+            roluser: true,
+            name: req.session.name,
+            rol: req.session.rol,
+            clientes:rows,
+            ruta:'clientes'
+        });
+    } }else{
+       
+            // Manejar el error apropiadamente
+            res.status(500).send('Error interno del servidor');
+        }
+    }
+ 
+    export const eliminarCliente = async (req, res) => {
+
+        if (req.session.rol == 'admin') {
+            const { id } = req.params;
+            const [result]=await pool.query('DELETE FROM customer WHERE id=?',[id])
+        //otro if de si es mayor a 0?
+        if (result && result.affectedRows > 0) {
+            const [rows]=await pool.query('SELECT * FROM customer');
+        res.render('clientes', {
+            alert: true,
+            alertTitle: "Eliminado",
+            alertMessage: "¡Eliminado Exitoso",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            login: true,
+            roluser: true,
+            name: req.session.name,
+            rol: req.session.rol,
+            clientes:rows,
+            ruta:'clientes'
+        });
+    } }else{
         (error) 
             console.error(error);
             // Manejar el error apropiadamente
@@ -87,5 +174,9 @@ export const editarUsuario = async (req, res) => {
 export const methods = {
     usuarios,
     editarUsuario,
-    eliminarUsuario
+    eliminarUsuario,
+    crearCliente,
+    editarClientes,
+    eliminarCliente,
   }
+
