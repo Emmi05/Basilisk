67a869cc68be2904c42abb5a9e8c188155fd8e58
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2024-03-23 22:34:38 -0600

    ediatr venta hecho validado

--- controller/controlador.js
+++ controller/controlador.js
@@ -1303,25 +1303,25 @@ const crearVenta = async (req, res) => {
 
 
 const editarVenta = async (req, res) => {
+    // re dirigir
     try {
         const { id } = req.params;
+        const [rows] = await pool.query('SELECT c.name as customer_name, c.a_paterno as customer_paterno, c.a_materno as customer_materno, l.lote, l.manzana, l.precio, l.id_interno, s.fecha_venta, s.n_cuentas, s.inicial, s.tipo_venta, s.cuotas, s.id FROM sale s JOIN customers c ON s.id_customer = c.id JOIN land l ON s.id_land = l.id WHERE s.id = ?', [id]);
+
         const { tipo_venta, inicial, n_cuentas, cuotas } = req.body;
+        const inicialNumber = parseFloat(inicial);
+        const nCuentasNumber = parseFloat(n_cuentas);
+
         let result = null;
-        // console.log(req.body);
 
         // Obtener el id_terreno_asociado
         const [venta] = await pool.query('SELECT id_land FROM sale WHERE id = ?', [id]);
         const id_terreno_asociado = venta[0].id_land;
-        console.log(venta);
-        console.log(id_terreno_asociado);
-
+     
         // Obtener el estado actual del terreno
-        const [terreno] = await pool.query('SELECT estado FROM land WHERE id = ?', [id_terreno_asociado]);
+        const [terreno] = await pool.query('SELECT estado, precio FROM land WHERE id = ?', [id_terreno_asociado]);
         const estado_terreno = terreno[0].estado;
-        console.log(terreno);
-
-        console.log("tipo_venta:", tipo_venta);
-        console.log("estado_terreno:", estado_terreno);
+        const precio_terreno = terreno[0].precio;
 
         if (tipo_venta === 'contado' && estado_terreno !== 'pagado') {
             // Actualizar los datos normales y establecer los valores de "inicial", "n_cuentas" y "cuotas" como null
@@ -1329,10 +1329,63 @@ const editarVenta = async (req, res) => {
                 'UPDATE sale SET tipo_venta = ?, inicial = NULL, n_cuentas = NULL, cuotas = NULL WHERE id = ?',
                 [tipo_venta, id]
             );
-            console.log(result);
+
             // Marcar el terreno como "pagado"
             await pool.query('UPDATE land SET estado = ? WHERE id = ?', ['pagado', id_terreno_asociado]);
         } else if (tipo_venta === 'credito') {
+             // if(inicialNumber <= 0 || !cantidades.test(inicial) || nCuentasNumber <= 0 || !cantidades.test(n_cuentas) || inicialNumber >= precio_terreno) {
+            if((inicialNumber >= precio_terreno)) {
+                return res.render('ventaEdit', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "El enganche debe ser menor que el precio del terreno.",
+                    alertIcon: 'error',
+                    showConfirmButton: false,
+                    timer: 3500,
+                    ruta: '/',
+                    login: true,
+                    roluser: false,
+                    name: req.session.name,
+                    rol: req.session.rol,
+                    ventas: rows,
+                    terrenos2: terreno,
+                });
+            }
+            if(inicialNumber <= 0 || !cantidades.test(inicial)) {
+                return res.render('ventaEdit', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Los valores de inicial son inválidos. Deben ser mayor a 0 y sin caracteres especiales.",
+                    alertIcon: 'error',
+                    showConfirmButton: false,
+                    timer: 3500,
+                    ruta: '/',
+                    login: true,
+                    roluser: false,
+                    name: req.session.name,
+                    rol: req.session.rol,
+                    ventas: rows,
+                    terrenos2: terreno,
+                });
+            }
+            if(nCuentasNumber <= 0 || !cantidades.test(n_cuentas)) {
+                return res.render('ventaEdit', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Los valores de n_cuentas son inválidos. Deben ser mayor a 0 y sin caracteres especiales.",
+                    alertIcon: 'error',
+                    showConfirmButton: false,
+                    timer: 3500,
+                    ruta: '/',
+                    login: true,
+                    roluser: false,
+                    name: req.session.name,
+                    rol: req.session.rol,
+                    ventas: rows,
+                    terrenos2: terreno,
+                });
+            }
+
             // Si es "crédito", actualiza los valores normales
             result = await pool.query(
                 'UPDATE sale SET tipo_venta = ?, inicial = ?, n_cuentas = ?, cuotas = ? WHERE id = ?',
@@ -1343,25 +1396,24 @@ const editarVenta = async (req, res) => {
             await pool.query('UPDATE land SET estado = ? WHERE id = ?', ['proceso', id_terreno_asociado]);
         }
     
-            const [rows] = await pool.query('SELECT * FROM sale');
-            res.render('venta', {
-                alert: true,
-                alertTitle: "Actualización",
-                alertMessage: "¡Actualización Exitosa!",
-                alertIcon: 'success',
-                showConfirmButton: false,
-                timer: 1500,
-                login: true,
-                roluser: false,
-                name: req.session.name,
-                rol: req.session.rol,
-                ventas: rows,
-                ruta: 'abonos'
-            });
+        
+        res.render('venta', {
+            alert: true,
+            alertTitle: "Actualización",
+            alertMessage: "¡Actualización Exitosa!",
+            alertIcon: 'success',
+            showConfirmButton: false,
+            timer: 1500,
+            login: true,
+            roluser: false,
+            name: req.session.name,
+            rol: req.session.rol,
+            ventas: rows,
+            ruta: 'abonos'
+        });
         
     } catch (error) {
         console.error("Error al actualizar la venta:", error);
-        // Manejar el error aquí
         res.status(500).send('Error al actualizar la venta');
     }
 };
