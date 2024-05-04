2be8c042c334bf3daae12bb980169b10f5526f5c
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2024-05-04 02:00:32 -0600

    pdf?

--- controller/pdf.js
+++ controller/pdf.js
@@ -7,6 +7,7 @@ const crearPdf = async (req, res) => {
     const id_venta = req.params.id;
 
     try {
+        // Realiza la consulta SQL para obtener la información de la venta y los abonos
         const [rows] = await pool.query('SELECT s.*, c.name AS customer_name, c.a_materno, c.a_paterno, l.lote, l.manzana, l.precio, a.fecha_abono, a.cuotas_pagadas, a.cuotas_restantes, a.n_abono, a.cantidad FROM sale s JOIN abonos a ON s.id = a.id_sale JOIN customers c ON s.id_customer = c.id JOIN land l ON s.id_land = l.id WHERE s.id = ?', [id_venta]);
         
         // Inicializa el documento PDF
@@ -19,6 +20,8 @@ const crearPdf = async (req, res) => {
         const imgX = doc.page.width - imgWidth - 30;
         const imgY = 10;
         doc.image('./public/img/logo.png', imgX, imgY, { width: imgWidth, height: imgHeight });
+        
+    // Agregar (fecha de generación del PDF)
         const fechaActual = new Date().toLocaleDateString('es-ES', {
             day: '2-digit',
             month: 'long',
@@ -27,6 +30,7 @@ const crearPdf = async (req, res) => {
             minute: '2-digit',
             second: '2-digit'
         });
+        // Agrega espacio y texto de descripción
         doc.moveDown();
         doc.moveDown();
         const lorem = `A continuación se mostrarán todos los abonos que se han proporcionado desde la fecha de compra - venta del terreno. (Esto solo es un historial) creado el día ${fechaActual}`;
@@ -272,7 +276,8 @@ const proceso = async (req, res) => {
          // Agrega datos de ventas de terrenos a la tabla
         for (let i = 0; i < rows.length; i++) {
             const venta = rows[i];
-            // console.log('Lote:', venta.lote, 'Manzana:', venta.manzana); //
+            console.log('Lote:', venta.lote, 'Manzana:', venta.manzana); // Agrega este console.log para verificar los valores de lote y manzana
+            // Resto del código para construir las filas de la tabla...
         }
 
         // Agregar (fecha de generación del PDF)
@@ -287,12 +292,12 @@ const proceso = async (req, res) => {
         // Agrega espacio y texto de descripción
         doc.moveDown();
         doc.moveDown();
-        const lorem = `A continuación se mostrarán todos los terrenos que se han vendido a credito (Esto solo es un historial) creado el día ${fechaActual}`;
+        const lorem = `A continuación se mostrarán todos los terrenos que se han vendido a contado (Esto solo es un historial) creado el día ${fechaActual}`;
         doc.text(` ${lorem}`, { align: 'justify' });
         doc.moveDown();
     
         const table = {
-            title: "Terrenos Vendidos credito",
+            title: "Terrenos Vendidos contado",
             subtitle: "Historial de compras a contado",
             headers: [
                 { label: "Fecha", property: 'fecha_venta', width: 70 },
@@ -596,21 +601,28 @@ const pagados = async (req, res) => {
 
 const finiquito =async (req,res)=>{
     const id_venta = req.params.id;
+
     const [informacion]=await pool.query('SELECT  s.id AS venta_id, MAX(a.fecha_abono) AS ultima_fecha_abono,  c.name AS customer_name, c.a_paterno AS customer_paterno, c.a_materno AS customer_materno, l.lote AS land_lote, l.manzana AS land_manzana, l.predial AS land_predial, l.id_interno AS land_id_interno FROM  sale s JOIN  abonos a ON s.id = a.id_sale JOIN  customers c ON s.id_customer = c.id JOIN  land l ON s.id_land = l.id WHERE  s.id = ? ORDER BY  a.fecha_abono DESC LIMIT 1;',  [id_venta]);
+   
     try{
+
+   console.log(informacion)
+
     // Inicializa el documento PDF
     const doc = new PDFDocument();
     const buffers = [];
-
-    const imgWidth = 100;
-    const imgHeight = 80; 
-    const imgX = doc.page.width - imgWidth - 30;
-    const imgY = 10; 
-    doc.image('./public/img/logo.png', imgX, imgY, { width: imgWidth, height: imgHeight });
+ 
+      // Establecer la posición de la imagen
+      const imgWidth = 100;
+      const imgHeight = 80; 
+      const imgX = doc.page.width - imgWidth - 30;
+      const imgY = 10; 
+      doc.image('./public/img/logo.png', imgX, imgY, { width: imgWidth, height: imgHeight });
 
       // Obtenemos la fecha en formato de objeto Date
     const fechaAbono = new Date(informacion[0].ultima_fecha_abono);
 
+    // Formateamos la fecha en letras
     const fechaFormateada = formatFechaEnLetras(fechaAbono);
     
     const tipo = `CARTA FINIQUITO`;
@@ -626,19 +638,22 @@ const finiquito =async (req,res)=>{
       doc.moveDown();
       doc.moveDown();
     
+       
         const lorem = ' apoderado de Basilisk Inmobiliaria Siete S de RL de CV, personalidad y facultades que acredito mediante escritura pública número 45,153 de 19 de Febrero de 2015, otorgada ante la fe del licenciado José Luis Altamirano Quintero, Notario Público número 66 del Distro Federal.';
         doc.text(`Israel Nogueda Pineda, ${lorem}`, {
-                align: 'justify'
-            });
+            // width: 410,
+            align: 'justify'
+        });
 
         doc.moveDown();
         doc.moveDown();
 
-        const finiquito = `Por este conducto manifiesto, a nombre de mi representada, que  C. ${informacion[0].customer_name}  ${informacion[0].customer_paterno} ${informacion[0].customer_materno} ha cumplido a plena satisfacción de mi representada con los pagos convenidos en el contrato privado de compra venta establecido entre las partes respecto al inmueble con clave  ${informacion[0].land_id_interno}, manzana  ${informacion[0].land_manzana} lote  ${informacion[0].land_lote}, ubicado en el Fraccionamiento Fuerza Área Mexicana, municipio de Acapulco de Juárez, Estado de Guerrero.   `;
+        const finiquito = `Por este conducto manifiesto, a nombre de mi representada, que  C. ${informacion[0].customer_name}  ${informacion[0].customer_paterno} ${informacion[0].customer_materno} ha cumplido a plena satisfacción de mi representada con los pagos convenidos en el contrato privado de compra venta establecido entre las partes respecto al inmueble con clave  ${informacion[0].land_id_interno}, manzana  ${informacion[0].land_manzana} lote  ${informacion[0].land_lote}, ubicado en el Fraccionamiento Fuerza Área Mexicana, municipio de Acapulco de Juáres, Estado de Guerrero.   `;
 
         doc.text(finiquito, {align: 'justify'});
         doc.moveDown();
     
+               // Agregar el párrafo del cliente
         const texto = `De conformidad con la claúsula QUINTA Y SEXTA del Contrato, en cuestión, Basilisk Inmobiliaira Siete S de RL de CV, ratifica, la obligatoriedad de firmar la escritura pública ante la fe del Notario Público que haya sido elegido por C. ${informacion[0].customer_name}  ${informacion[0].customer_paterno} ${informacion[0].customer_materno}, obligándose a entregar la documentación necesaria para la celebración de la misma, quedando a cargo de C. ${informacion[0].customer_name}  ${informacion[0].customer_paterno} ${informacion[0].customer_materno} los honorarios, gastos, impuestos y derechos que genere la escritura pública, así como también todos los gastos e impuestos anteriores y posteriores por concepto de predial con cuenta catastral   ${informacion[0].land_predial} y derechos por servicios de agua, luz o cualquier otro servicio que se tenga que cubrir por lote materia de compraventa, para llevar a cabo la escrituración respectiva.   `;
 
         doc.text(texto, {align: 'justify'});
