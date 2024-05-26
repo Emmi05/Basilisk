5294f3cde5ef45a438b15fac7bbce929b9cec2e8
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2024-05-25 20:24:49 -0600

    por si no lo encuentra?

--- app.js
+++ app.js
@@ -51,6 +51,15 @@ app.use((err, req, res, next) => {
     console.error('Error de servidor:', err.stack);
     res.status(500).render('500');
 });
+// Servir manifest.json (si no está ya en la carpeta 'public')
+app.get('/manifest.json', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'manifest.json'));
+});
+
+// Servir service-worker.js (si no está ya en la carpeta 'public')
+app.get('/service-worker.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'service-worker.js'));
+});
 
 const PORT = process.env.PORT || 3000;
 
