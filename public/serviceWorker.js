37390c09c6e4c6f646085f2a9419121be902b614
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2024-05-25 20:38:57 -0600

    prueba 05?

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -45,4 +45,26 @@ self.addEventListener('install', event => {
         })
     );
   });
- 
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request)
+        .then(response => {
+          return response || fetch(event.request);
+        })
+    );
+  });
+  
+  self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
